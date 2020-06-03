e437a3204fa5aa8dd776f2800b5a1d4f1078ae9b
xushalx <xushalx@gmail.com>
2020-06-02 20:26:27 -0700

    random color per player

--- server.js
+++ server.js
@@ -2,19 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/static"));
-// app.set('view engine', 'ejs');
-// app.set('views', __dirname + '/views');
 
-// const session = require('express-session');
-// app.set('trust proxy', 1) // trust first proxy
-// app.use(session({
-//   secret: 'keyboardkitteh',
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: { maxAge: 60000 }
-// }))
-
-// const server = app.listen(8000);
 const server = require('http').createServer(app);
 server.listen(8000);
 const io = require('socket.io')(server);
@@ -24,30 +12,26 @@ var players={};
 io.on('connection', function (socket) { //2
     socket.on('got_new_user', function(data){
         socket.emit('existing_users',users)
-        // console.log("In server-socket.emit")
-        // console.log(data.name)
         if (data.name){
             users[socket.id]=data.name
-            // console.log("new user**"+data)
             io.emit('new_user', {name:data.name, id:socket.id})
             console.log(users)
             console.log(socket.id)
             players[socket.id]={
                 x:100,
-                y:100
+                y:100,
+                color:getRandomColor()
             }
             console.log(socket.id+"$$$$$$$$")
         }
-
     })
    socket.on('disconnect', function(){
        console.log("disconnected id:",socket.id)
        io.emit('disconnected_user',socket.id)
        delete users[socket.id]
-    //    console.log(users)
+       delete players[socket.id]
    })
    socket.on('new_msg', function(data){
-    //    console.log("new_msg: "+data)
         io.emit('new_message', { name: users[socket.id], msg: data})
    })
 
@@ -55,7 +39,6 @@ io.on('connection', function (socket) { //2
         console.log("In socket.on movement")
         console.log(players[socket.id]);
         var player = players[socket.id] || {};
-        // console.log(player.x,"------", player.y, users[socket.id]);
         if (data.left ) {
             if(player.x>0){
                 player.x -= 5;
@@ -89,21 +72,23 @@ io.on('connection', function (socket) { //2
             }
         }
     });
-
 });
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
 setInterval(function() {
-    // console.log("in state resresh")
     io.sockets.emit('state', players);
-  }, 1000/60);
+  }, 1000/100);
 
 
 // Routing
-// app.get('/',(request,response)=>{
-//     console.log("*********************** in root")
-//     response.render('index')
-// })
 app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname, 'index.html'));
   });
-
--- static/chat.js
+++ static/chat.js
@@ -1,22 +1,17 @@
 $(document).ready(function (){
-    const socket = io(); //1
+    const socket = io(); 
     let name= prompt("Enter your preferred name!");
     socket.emit('got_new_user',{name:name});
     socket.on('existing_users',function(data){
-        // console.log(data)
         for(let i in data ){
             console.log("this is socket id:",i)
             $("#sidebar").append('<p id='+i+'>'+data[i]+'</p>')
         }
     })
     socket.on('new_user',function(data){
-        // console.log("newwwwww"+data)
         $("#sidebar").append('<p id='+data.id+'>'+data.name+"</p>")
     })
     socket.on('disconnected_user',function(data){
-        // console.log("A disconnect happened--------")
-        // console.log("in disconnected",data)
-        // console.log('#'+data)
         $('#'+data).remove();
     })
     $("#message").keypress(function(event){            
@@ -33,7 +28,6 @@ $(document).ready(function (){
         $("#message").val('')
     })
     socket.on('new_message',function(data){
-        // console.log("new message reemmited by server: " +data.name+"  "+ data.msg)
         $("#chat_box").append("<p>"+data.name+":  "+data.msg+"</p>")
         $("#chat_box").stop().animate({scrollTop: $("#chat_box")[0].scrollHeight},1000);
     })
@@ -64,23 +58,15 @@ $(document).ready(function (){
     document.addEventListener('keyup', function(event) {
         switch (event.keyCode) {
             case 65: // A
-            console.log("pressed A");
-            console.log(socket.id+"#########")
             movement.left = false;
             break;
             case 87: // W
-            console.log("pressed W");
-            console.log(socket.id+"#########")
             movement.up = false;
             break;
-            case 68: // D
-            console.log("pressed D");
-            console.log(socket.id+"#########")
+            case 68: // D            
             movement.right = false;
             break;
             case 83: // S
-            console.log("pressed S");
-            console.log(socket.id+"#########")
             movement.down = false;
             break;
         }
@@ -88,24 +74,22 @@ $(document).ready(function (){
     
     setInterval(function() {
         socket.emit('movement', movement);
-        }, 1000/60);
+        }, 1000/100);
     
     var canvas = document.getElementById('myCanvas');
     canvas.width=500;
     canvas.height=400;
-    console.log(canvas)
     var context = canvas.getContext('2d');
-    console.log(context);
     socket.on('state', function(players) {
-        // console.log("CheckCheck")
         context.clearRect(0, 0, 500, 400);
-        context.fillStyle = "#FF0000";
         for (var id in players) {
             var player = players[id];
             console.log(player+"==========");
+            context.fillStyle =player.color ;
             context.beginPath();
             context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
             context.fill();
+            context.stroke();
         }
     });
  })
